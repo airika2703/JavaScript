a39bd969d78fe758985f09aa90767e11d62a4f48
Airikakhang <khangskt111@gmail.com>
2021-08-01 18:42:30 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,50 @@
+// Khai báo biến 
+var fullName = 'Le Hoang Khang';
+var age = 26;
+
+// Gọi hàm thông báo
+
+// alert(fullName);
+// alert(age);
+
+// Comments
+
+// Mot so ham built-in
+/*
+Giới thiệu một số built-in 
+    1. Alert
+    2. Console
+    3. Confirm
+    4. Prompt
+    5. Set timeout
+    6. Set interval
+*/
+
+
+// Toán tử (giới thiệu)
+/*
++       -->    cộng
+-       -->    trừ
+*       -->    nhân
+**      -->    lũy thừa
+/       -->    chia
+%       -->    chia lấy sô dư
+++      -->    tăng 1 giá trị sô
+--      -->    giảm 1 giá trị số
+*/
+
+
+/**
+ * 0
+ * false
+ * '' _ ""
+ * underined
+ * NaN
+ * null
+ */
+
+/** Toán tử logic
+ * 1. && - and
+ * 2. || - or
+ * 3. ! - not
+ */
